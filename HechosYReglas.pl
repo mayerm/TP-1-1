2b8f1631278bf248ab2d56a65c2182f0aafc91f7
mayerm <mayer.marcos@yahoo.com.ar>
2017-03-08 10:18:34 -0300

    Source ProLog

---
+++ HechosYReglas.pl
@@ -0,0 +1,25 @@
+persona(juan).
+persona(jesus).
+persona(alicia).
+persona(ezequiel).
+persona(victoria).
+persona(luis).
+padre(juan,jesus).
+padre(alicia,jesus).
+padre(ezequiel,juan).
+
+
+hermano(A,B) :-
+	padre(A,P),
+	padre(B,P),
+	A \== B.
+nieto(A,B) :-
+	padre(A,P),
+	padre(P,B).
+sin_parentezco(A,B) :-
+	persona(A),
+	persona(B),
+	not(padre(A,B)),
+	not(padre(B,A)),
+	not(nieto(A,B)),
+	not(hermano(A,B)).
